9a609dcdb1db9de496e5c2bc47c14dce180d90c3
Tsvetelina Gadzhelova <tsvetelina.gadzhelova@gmail.com>
2026-06-26 19:06:01 +0300

    added icons

---
+++ sw.js
@@ -0,0 +1,30 @@
+// Simple offline cache for the Cringe Button PWA.
+const CACHE = 'cringe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
